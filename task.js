4dc224ee2f7fc82245453b652ef1697be76748dc
Elvehnn <70837634+Elvehnn@users.noreply.github.com>
2021-03-28 20:06:37 +0300

    Add files via upload

--- task.js
+++ task.js
@@ -1,94 +1,90 @@
-var Calculator = function() {
-    result = 0;
-  
-    this.getResult = function () {
-      return this.result;
-    };
-  
-    this.reset = function () {
-      this.result = 0;
-      return this.result;
-    };
-      
-    this.add = function(a) {
-      var acc;
-      if (a === undefined) {
-        acc = 0;
-      } else {
-        acc = this.result + a;
-        this.result = acc;
-      }
-      let n = (b) => {
-        if (b !== undefined) {
-          acc += b;
-          this.result = acc;
-        } else {
-          acc = 0;
-        }
-      return n;
-      }
-      return n;
-    };
+const calculator = {
+  result: 0,
+
+  getResult: function() {
+    return calculator.result;
+  },
+
+  reset: function() {
+    calculator.result = 0;
+    return calculator.result;
+  },
     
-    this.subtract = function(a) {
-      var acc;
-      if (a === undefined) {
-        acc = 0;
+  add: function(a) {
+    var acc = calculator.result;
+    if (a !== undefined) {
+      acc += a;
+      calculator.result = acc;
+    }
+    let n = (b) => {
+      if (b !== undefined) {
+        acc += b;
+        calculator.result = acc;
+      } 
+    return n;
+    }
+    return n;
+  },
+
+  subtract: function(a) {
+    var acc;
+    if (a === undefined) {
+      acc = 0;
+    } else {
+      acc = calculator.result - a;
+      calculator.result = acc;
+    }
+    let n = (b) => {
+      if (b !== undefined) {
+        acc -= b;
+        calculator.result = acc;
       } else {
-        acc = this.result - a;
-        this.result = acc;
-      }
-      let n = (b) => {
-        if (b !== undefined) {
-          acc -= b;
-          this.result = acc;
-        } else {
-          acc = 0;
-        }
-      return n;
-      }
-      return n;
-    };
-    
-    this.divide = function(a) {
-      var acc;
-      if (a === undefined) {
         acc = 0;
+      }
+    return n;
+    }
+    return n;
+  },
+
+  divide: function(a) {
+    var acc;
+    if (a === undefined) {
+      acc = 0;
+    } else {
+      acc = calculator.result / a;
+      calculator.result = acc;
+    }
+    let n = (b) => {
+      if (b !== undefined) {
+        acc = acc / b;
+        calculator.result = acc;
       } else {
-        acc = this.result / a;
-        this.result = acc;
+        acc = 0;
       }
-      let n = (b) => {
-        if (b !== undefined) {
-          acc = acc / b;
-          this.result = acc;
-        } else {
-          acc = 0;
-        }
-      return n;
+    return n;
+    }
+    return n;
+  },
+
+  multiply: function(a) {
+    var acc;
+    if (a === undefined) {
+      acc = 0;
+    } else {
+      acc = calculator.result * a;
+      calculator.result = acc;
+    }
+    let n = (b) => {
+      if (b !== undefined) {
+        acc *= b;
+        calculator.result = acc;
+      } else {
+        acc = 0;
       }
-      return n;
-    };
+    return n;
+    }
+    return n;
+  },
+} 
   
-    this.multiply = function(a) {
-      var acc;
-      if (a === undefined) {
-        acc = 0;
-      } else {
-        acc = this.result * a;
-        this.result = acc;
-      }
-      let n = (b) => {
-        if (b !== undefined) {
-          acc = acc * b;
-          this.result = acc;
-        } else {
-          acc = 0;
-        }
-      return n;
-      }
-      return n;
-    };
-  }
-  
-  module.exports = Calculator;
+  module.exports = calculator;
